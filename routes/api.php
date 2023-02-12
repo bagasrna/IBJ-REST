77611083d942137b543571f86a277aadb62da84c
Bagas Raditya Nafi <bagasrnfull@gmail.com>
2023-02-12 12:28:33 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth'],function (){
